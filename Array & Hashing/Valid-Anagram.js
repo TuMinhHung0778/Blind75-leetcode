d5b738721a0afe091c3342c6302c85675b3329c9
TuMinhHung0778 <tuminhhung0901@gmail.com>
2025-09-10 05:07:39 +0700

    valid-anagram

--- Array & Hashing/Valid-Anagram.js
+++ Array & Hashing/Valid-Anagram.js
@@ -1,8 +1,70 @@
+// Valid Anagram
+
+// Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+// An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+// Example 1:
+
+// Input: s = "racecar", t = "carrace"
+
+// Output: true
+// Example 2:
+
+// Input: s = "jar", t = "jam"
+
+// Output: false
+// Constraints:
+
+// s and t consist of lowercase English letters.
+
 class Solution {
     /**
      * @param {string} s
      * @param {string} t
      * @return {boolean}
      */
-    isAnagram(s, t) {}
+    isAnagram(s, t) {
+        let s1 = s.split('').sort().join('');
+        let s2 = t.split('').sort().join('');
+
+        if (s1 === s2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+// use Bubble Sort
+class Solution2 {
+    // Input: s = "jar", t = "jam"
+    isAnagram(s, t) {
+    }
+
+}
+
+// function bubbleSort(arr) {
+//   let n = arr.length;
+//   for (let i = 0; i < n - 1; i++) {
+//     for (let j = 0; j < n - i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         // Hoán đổi vị trí
+//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//       }
+//     }
+//   }
+//   return arr;
+// }
+// use Bubble Sort
+function bubbleSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+    return arr;
 }
